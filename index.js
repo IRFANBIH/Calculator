51000fd3f3fbd8c8ec0cdf914e2197d7c9a7b475
irfan <ipekusic@yahoo.com>
2023-01-17 11:50:06 -0500

    added variables

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equal]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const oldOperation = document.querySelector('[data-old]')
+const nowOperation = document.querySelector('[data-now]')
